e330c16fa187aaf3f7efa8b56c1750108b6ece6c
aadipandagle <adi.pandagle@gmail.com>
2023-10-08 21:55:28 +0530

    HomePage updated, header menu icon changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,31 +17,30 @@ const Navbar = () => {
             <div className="flex items-center justify-end w-full px-4">
               <div>
                 <button
-                  // @click="navbarOpen = !navbarOpen"
                   onClick={() => setOpen(!open)}
-                  // :className="navbarOpen && 'navbarTogglerActive' "
                   id="navbarToggler"
                   className={` ${open && "navbarTogglerActive"
                     } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
                 >
-                  <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
-                  <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
-                  <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M4 6l16 0" />
+                    <path d="M4 12l16 0" />
+                    <path d="M4 18l16 0" />
+                  </svg>
                 </button>
                 <nav
-                  // :className="!navbarOpen && 'hidden' "
                   id="navbarCollapse"
-                  className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white py-5 px-6 shadow lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none ${!open && "hidden"
+                  className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white py-5 px-6 shadow lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none z-20 ${!open && "hidden"
                     } `}
                 >
                   <ul className="block lg:flex">
-                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' to="/">Home</Link>
-                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' to="/category">Category</Link>
-                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' to="/press-release">Press Release</Link>
-                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' to="/offering">Offering</Link>
-                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' to="/contact">Contact</Link>
-                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' to="/about">About</Link>
-                    {/* <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' to="/">Home</Link> */}
+                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' onClick={() => setOpen(!open)} to="/">Home</Link>
+                    {/* <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' onClick={() => setOpen(!open)} to="/category">Category</Link>
+                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' onClick={() => setOpen(!open)} to="/press-release">Press Release</Link>
+                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' onClick={() => setOpen(!open)} to="/offering">Offering</Link> */}
+                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' onClick={() => setOpen(!open)} to="/contact">Contact</Link>
+                    <Link className='flex py-2 text-base font-medium lg:ml-12 lg:inline-flex' onClick={() => setOpen(!open)} to="/about">About</Link>
                   </ul>
                 </nav>
               </div>
