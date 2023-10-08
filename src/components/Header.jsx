fba1871e3a80924710d0b5f0554d1a971d7da957
siddhant.pandagale <adi.pandagle@gmail.com>
2023-10-08 22:03:11 +0530

    Minor bug fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="container">
           <div className="relative flex items-center justify-between mx-4">
             <div className="max-w-full px-4 w-60">
-              <div className=" w-full py-5 text-2xl">
+              <div className="w-full py-5 text-2xl ">
                 Congruence
               </div>
             </div>
@@ -22,7 +22,7 @@ const Navbar = () => {
                   className={` ${open && "navbarTogglerActive"
                     } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M4 6l16 0" />
                     <path d="M4 12l16 0" />
