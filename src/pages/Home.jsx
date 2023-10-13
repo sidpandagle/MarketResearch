7edee1fdb6f4cf5bc441646a620f7cb8d5c66f30
siddhant.pandagale <adi.pandagle@gmail.com>
2023-10-14 00:28:57 +0530

    Latest Reports added, footer changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Search from '../components/Search'
 import Testimonial from '../components/Testimonial'
 import Clients from '../components/Clients'
 import AboutHome from '../components/AboutHome'
+import LatestReports from '../components/LatestReports'
 
 export default class Home extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class Home extends Component {
       <section className="relative">
         <Search />
         {/* <AboutHome /> */}
+        <LatestReports />
         <Testimonial />
         <Clients />
       </section>
